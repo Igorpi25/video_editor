19a0faf09d0623d75fae16ffe1b38e88f6c72af0
Igor Ivanov <igorpi25@gmail.com>
2019-04-04 22:28:07 +0600

    [ADDED] video-trimming-android project as a dependency to Android

--- android/app/src/main/kotlin/videoeditor/com/ivanov/tech/videoeditor/MainActivity.kt
+++ android/app/src/main/kotlin/videoeditor/com/ivanov/tech/videoeditor/MainActivity.kt
@@ -23,10 +23,22 @@ class MainActivity : FlutterActivity() {
                 val json: String? = call.argument("json")
                 result.success("recieved in Android")
                 Log.d("Igor", "MethodChannel showNativeView json=$json")
+            } else if (call.method == "video_trim") {
+                val path: String? = call.argument("path")
+                Log.d("Igor", "MethodChannel video_trim source path=$path")
+
+                val result_path: String? = trimVideo(path)
+
+                Log.d("Igor", "MethodChannel video_trim result path=$result_path")
+                result.success(result_path)
             } else {
                 Log.d("Igor", "MethodChannel notImplemented")
                 result.notImplemented()
             }
         }
+    }
+
+    fun trimVideo(path: String?): String? {
+        return path
     }
 }
