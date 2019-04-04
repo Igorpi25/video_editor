e9d84b9b309baa0f63c7970d8fdec1d288bd70a5
Igor Ivanov <igorpi25@gmail.com>
2019-04-04 15:09:30 +0600

    [ADDED] Interaction with Android native code is checked with send_json.dart

--- android/app/src/main/kotlin/videoeditor/com/ivanov/tech/videoeditor/MainActivity.kt
+++ android/app/src/main/kotlin/videoeditor/com/ivanov/tech/videoeditor/MainActivity.kt
@@ -1,13 +1,32 @@
 package videoeditor.com.ivanov.tech.videoeditor
 
 import android.os.Bundle
+import android.util.Log
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+
+    companion object {
+        const val CHANNEL = "flutter.rortega.com.channel"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "showNativeView") {
+
+                val json: String? = call.argument("json")
+                result.success("recieved in Android")
+                Log.d("Igor", "MethodChannel showNativeView json=$json")
+            } else {
+                Log.d("Igor", "MethodChannel notImplemented")
+                result.notImplemented()
+            }
+        }
+    }
 }
